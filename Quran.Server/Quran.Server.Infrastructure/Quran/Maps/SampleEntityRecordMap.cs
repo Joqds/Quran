b769eea4487af30550dbaff3625c6ef297a84055
Mohsen Meshkini <mmeshkiny@yahoo.com>
2021-07-23 05:44:34 +0430

    rename typo Infrastructure

---
+++ Quran.Server/Quran.Server.Infrastructure/Quran/Maps/SampleEntityRecordMap.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quran.Server.Infrastructure.Quran.Maps
+{
+    public class SampleEntityRecordMap
+    {
+    }
+}
