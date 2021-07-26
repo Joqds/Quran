8d90a9ae8d162b5e5cedfda1cc02bad0eae730e7
Mohsen Meshkini <mmeshkiny@yahoo.com>
2021-07-26 13:49:01 +0430

    seeding history migration

--- Quran.Server/Quran.Server.Infrastructure/Migrations/20210726091829_Add_Seeding_Tracking.Designer.cs
+++ Quran.Server/Quran.Server.Infrastructure/Migrations/20210726091829_Add_Seeding_Tracking.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Quran.Server.Infrastructure.Persistence;
 
 namespace Quran.Server.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210726091829_Add_Seeding_Tracking")]
+    partial class Add_Seeding_Tracking
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
